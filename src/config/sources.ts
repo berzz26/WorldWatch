f12d03e2f1a2c937d45861d1b13bd4fb47813f59
berzz26 <aumtamboli2233@gmail.com>
2026-03-01 04:06:42 +0000

    feat: add sources

---
+++ src/config/sources.ts
@@ -0,0 +1,6 @@
+export const RSS_SOURCES = [
+  "https://nitter.net/ReutersWorld/rss",
+  "https://nitter.net/PentagonPresSec/rss"
+];
+
+export const REUTERS_WORLD = "https://www.reuters.com/world/";
